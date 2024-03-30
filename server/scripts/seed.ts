c2eed66bb92a1791fe2356346e8ee65ae7796ef0
Joebim <josey359@gmail.com>
2024-03-30 18:14:27 +0100

    [FIX] seeding error

--- server/scripts/seed.ts
+++ server/scripts/seed.ts
@@ -1,6 +1,8 @@
 import * as dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
 import { customSeed } from "./customSeed";
+import { Salt, parseSalt } from "../src/auth/password.service";
+import { hash } from "bcrypt";
 
 if (require.main === module) {
   dotenv.config();
@@ -10,9 +12,15 @@ if (require.main === module) {
   if (!BCRYPT_SALT) {
     throw new Error("BCRYPT_SALT environment variable must be defined");
   }
+  const salt = parseSalt(BCRYPT_SALT);
+
+  seed(salt).catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
 }
 
-async function seed() {
+async function seed(bcryptSalt: Salt) {
   console.info("Seeding database...");
 
   const client = new PrismaClient();
